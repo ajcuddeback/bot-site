01a25044b343d166b14d6559542553a6763e4108
ajcudd <ajcuddeback@gmail.com>
2021-05-29 03:29:07 -0400

    added html and js for bot to use

---
+++ assets/script.js
@@ -0,0 +1,66 @@
+const addBtn = document.querySelector('.add');
+const subtractBtn = document.querySelector('.subtract');
+const numberSpan = document.querySelector('.number');
+const form = document.querySelector('.form');
+const dataDiv = document.querySelector('.data');
+let currentNum = 0;
+let firstName = "";
+let job = "";
+let age = "";
+let dataArr = [];
+
+function setNumDiv() {
+    numberSpan.innerHTML = currentNum;
+}
+
+function addOne() {
+    currentNum += 1;
+    setNumDiv();
+}
+
+function subOne() {
+    currentNum -= 1;
+    console.log('click')
+    setNumDiv();
+}
+
+function submitForm(e) {
+    e.preventDefault();
+
+    firstName = document.querySelector('.name-input').value;
+    job = document.querySelector('.job-title').value;
+    age = document.querySelector('.age').value;
+
+    let dataObj = {
+        firstName: firstName,
+        job: job,
+        age: age
+    };
+
+    dataArr.push(dataObj);
+
+    displayData();
+}
+
+function displayData() {
+    while(dataDiv.firstChild) {
+        dataDiv.removeChild(dataDiv.firstChild)
+    }
+    dataArr.forEach(element => {
+        const nameElement = document.createElement("h2");
+        const jobElement = document.createElement("h2");
+        const ageElement = document.createElement("h2");
+
+        nameElement.innerHTML = element.firstName;
+        jobElement.innerHTML = element.job;
+        ageElement.innerHTML = element.age;
+
+        dataDiv.append(nameElement);
+        dataDiv.append(jobElement);
+        dataDiv.append(ageElement);
+    })
+}
+
+addBtn.addEventListener('click', addOne);
+subtractBtn.addEventListener('click', subOne);
+form.addEventListener('submit', submitForm);
